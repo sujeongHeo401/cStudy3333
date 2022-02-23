120f6b4022007b0ea3bb3473cc54a5f2372905f5
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-23 23:04:08 +0900

    3의 갯수 구하쟈

---
+++ 29.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+
+	int n, tmp, i, cnt = 0, digit;
+	
+	scanf_s("%d", &n);
+	
+	for (i = 1; i <= n; i++) {
+		tmp = i;
+		while (tmp > 0) {
+			digit = tmp % 10;
+			if (digit == 3) cnt++;
+			tmp = tmp / 10;
+		}
+	}
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
+
+///3의 갯수를 구하는 문제여따,,,,
