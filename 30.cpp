47b6a927bbf7fc08c6b4ece881c9a8da3b906d4b
sujeongHeo401 <gotnwjd40@gmail.com>
2022-03-07 21:59:45 +0900

    29번 3의 갯수 세기, 31번 탄소 뭉뎨

--- 30.cpp
+++ 30.cpp
@@ -1,13 +1,16 @@
 ﻿#include <stdio.h>
 #include <vector>
 #include <algorithm>
+
+//////////////5367
+/////////// 각 자리 수가 3보다 큰 경우, 3보다 작은 경우 3고과 같은 경우
 using namespace std;
 int main() {
 
 	int n, tmp, i, cnt = 0, digit;
-	
+
 	scanf_s("%d", &n);
-	
+
 	for (i = 1; i <= n; i++) {
 		/*tmp = i;
 		while (tmp > 0) {
---
+++ 31.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+    char a[10];
+    int c = 0, h = 0, i, pos;
+    scanf("%s", &a);
+    if (a[1] == 'H') {
+        c = 1;
+        pos = 1;
+    }
+    else {
+        for (i = 1; a[i] != 'H'; i++) {
+            c = 10 * c + (a[i] - 48);
+        }
+        pos = i; /// h 의 위치 
+    }
+    if (a[pos + 1] == '\0') h = 1;
+    else {
+        for (i = pos + 1; a[i] != '\0'; i++) {
+            h = h * 10 + (a[i] - 48);
+        }
+
+    }
+    printf("%d\n", c * 12 + h);
+
+    return 0;
+    /*else {
+        for(i = 1; a[i] != 'H'; i++)
+    }*/
+   
+}
